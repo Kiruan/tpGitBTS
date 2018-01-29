6c2740418054419cf7a764c4bce927e4bf2e051e
Kiruan <alois.van@gmail.com>
2018-01-29 16:56:01 +0100

    jbfeib

--- src/calculette/calculetteMain.java
+++ src/calculette/calculetteMain.java
@@ -1,9 +1,12 @@
 package src.calculette;
 
 public class calculetteMain {
-	int input1 = 0;
-	int input2 = 0;
-	char signe;
+	public static void main(string args) {
+		
+	}
+	int input1 = 5;
+	int input2 = ;
+	char signe = "/";
 	resultat = 0;
 	if(signe == "+") {
 		resultat = input1 + input2;
@@ -18,5 +21,6 @@ public class calculetteMain {
 		resultat = input1 / input2; 
 	}
 	printf(1, resultat, 24);
+	}
 }
 
